3ff9182899c08417e32edd606c99d31703704af4
Harshit Tiwari <104323483+harshit-tiwaari@users.noreply.github.com>
2023-02-14 19:09:16 +0530

    Add files via upload
    learn how to delete nd update mongodb data with nodejs

--- mongoose.js
+++ mongoose.js
@@ -1,18 +1,36 @@
 const mongoose = require('mongoose');
-
-const main=async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/e-comm")
-  const Productschema = new mongoose.Schema({
+mongoose.connect("mongodb://127.0.0.1:27017/e-comm")
+ const productSchema = new mongoose.Schema({
     name: String,
     price: Number,
     brand: String,
     Category: String
   })
-  const Product = mongoose.model("product", Productschema)
-  let data = new Product({ name: "iphone 14 pro",price:160000,brand:"Apple",Category:"mobile" })
+const saveindb = async () => {
+ const product = mongoose.model("product", productSchema)
+  let data = new mongoose.Schema({
+    name: String,
+    price: Number,
+    brand: String,
+    Category: String
+  })
+  
   let result = await data.save()
   console.log(result)
 }
-main()
+  const updateindb = async () => {
+  
+    const product = mongoose.model("product", productSchema)
+    let data = await product.updateOne({ name: "iphone 14 pro" },
+      { $set: { price: 18000 } })
+  
+  console.log(data)
+}
+updateindb() //a lot of questions?
 
-
+const deleteindb = async() => {
+  const product = mongoose.model('product', productSchema)
+  let data = await product.deleteOne({ name: 'iphone xr' })
+  console.log(data);
+}
+deleteindb()
